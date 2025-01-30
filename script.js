e8c20399fee42244b36832e1cad82c64e0ebfb78
Jerome <issa@A.J.Issa>
2025-01-30 11:17:08 +0100

    modify css and display elements from API

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
-// 1 - Tester le lien de l'API dans le navigateur (https://restcountries.com/v3.1/all)
+const container = document.querySelector(".countries-container");
+let country = [];
 
-// 2 - Créer une fonction pour "fetcher" les données, afficher les données dans la console.
+async function url() {
+  await fetch("https://restcountries.com/v3.1/all")
+    .then((res) => res.json())
+    .then((data) => (country = data));
+  console.log(country);
+  displayCard();
+}
 
+function displayCard() {
+  container.innerHTML = country
+    .map((countries) => {
+      return `
+        <div class="card">
+          <img class="flag" src="${countries.flags.png}" alt="${countries.name.official}" />
+          <h1>${countries.name.common}</h1>
+          <p class="capital">${countries.capital}</p>
+          <p class="pop">Population : ${countries.population}
+        </div>
+    `;
+    })
+    .join("");
+}
+url();
 // 3 - Passer les données à une variable
 
 // 4 - Créer une fonction d'affichage, et paramétrer l'affichage des cartes de chaque pays grace à la méthode MAP
 
 // 5 - Récupérer ce qui est tapé dans l'input et filtrer (avant le map) les données
-coutry.name.includes(inputSearch.value);
+// country.name.includes(inputSearch.value);
 
 // 6 - Avec la méthode Slice gérer le nombre de pays affichés (inputRange.value)
 
